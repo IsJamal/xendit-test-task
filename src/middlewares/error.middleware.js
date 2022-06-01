35f954345d149862559d5228bf849e204a348ca9
unknown <camal.ismayilzade@gmail.com>
2022-06-01 06:38:53 +0400

    added log to error handling

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,6 +1,8 @@
 const { AppError } = require('../helpers/errors');
+const logger = require('../helpers/logger');
 
 module.exports = (err, req, res, next) => {
+    logger.error(err.message);
     if (err instanceof AppError) {
         res.status(err.status).send(err.json());
     } else {
